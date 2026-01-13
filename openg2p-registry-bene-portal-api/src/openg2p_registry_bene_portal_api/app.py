179b34c62dc890d1acca3e3ded95f75edd0f2eaa
PSNAppZ <thepsnarayanan@gmail.com>
2026-01-13 10:49:17 +0530

    Remove Core and Extensions initializations from the BenePortal and Partner API Initializers

--- openg2p-registry-bene-portal-api/src/openg2p_registry_bene_portal_api/app.py
+++ openg2p-registry-bene-portal-api/src/openg2p_registry_bene_portal_api/app.py
@@ -18,8 +18,6 @@ _logger = logging.getLogger(_config.logging_default_logger_name)
 
 class Initializer(BaseInitializer):
     def initialize(self, **kwargs):
-        CoreInitializer().initialize()
-        ExtensionsInitializer().initialize()
         
         RequestResponseHelper()
         BenePortalController().post_init()
