e270fa2cf16fb3ed05f6b72169a5fdfd87db4c45
tahzeer <tahzeer.ash@gmail.com>
2026-05-14 16:19:47 +0530

    G2P-4807 feat(controller): add register semantic pattern endpoints
    - implement create, get, update, delete, and list endpoints
    - add request and response models for register semantic patterns

--- openg2p-registry-staff-portal-api/src/openg2p_registry_staff_portal_api/helpers/request_response_helper.py
+++ openg2p-registry-staff-portal-api/src/openg2p_registry_staff_portal_api/helpers/request_response_helper.py
@@ -36,6 +36,7 @@ from openg2p_registry_core.schemas import (
     IncomingPartnerData, IncomingPartnerResponseBody, IncomingPartnersResponseBody,
     IncomingModelKeyPathData, IncomingModelKeyPathResponseBody, IncomingModelKeyPathListResponseBody,
     IncomingModelSemanticPatternResponseBody, IncomingModelSemanticPatternsResponseBody,
+    IncomingModelRegisterSemanticPatternResponseBody, IncomingModelRegisterSemanticPatternsResponseBody,
     IncomingTemplateResponseBody, IncomingTemplatesResponseBody,
     DataModelResponseBody, DataModelsResponseBody, SubscriptionActivityLogsResponseBody,
     OutgoingTopicResponseBody, OutgoingTopicsResponseBody, OutgoingTemplateResponseBody, OutgoingTemplatesResponseBody,
@@ -1027,6 +1028,16 @@ class RequestResponseHelper(BaseService):
             )
         elif response_class_name == 'IncomingModelSemanticPatternsResponse':
             response_body = IncomingModelSemanticPatternsResponseBody(
+                response_payload=payload_data,
+                pagination_response=pagination_response,
+            )
+        elif response_class_name == 'IncomingModelRegisterSemanticPatternResponse':
+            response_body = IncomingModelRegisterSemanticPatternResponseBody(
+                response_payload=payload_data,
+                pagination_response=pagination_response,
+            )
+        elif response_class_name == 'IncomingModelRegisterSemanticPatternsResponse':
+            response_body = IncomingModelRegisterSemanticPatternsResponseBody(
                 response_payload=payload_data,
                 pagination_response=pagination_response,
             )
